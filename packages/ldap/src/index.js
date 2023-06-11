c12fe12ded5ed2c3c8417260ef3d3a67c5118c4f
whidix <theodor.vallier@gmail.com>
2023-06-11 22:37:34 +0200

    feat: add groups

--- packages/ldap/src/index.js
+++ packages/ldap/src/index.js
@@ -212,6 +212,232 @@ server.search(`ou=people,${rootDn}`, async (req, res, next) => {
   }
 });
 
+server.search(`ou=ecoles,ou=groups,${rootDn}`, async (req, res, next) => {
+  console.log(`ou=ecoles,ou=clubs,${rootDn} ` + req.scope);
+  try {
+    if (req.filter instanceof ldap.PresenceFilter && req.scope === 'base') {
+      res.send({
+        dn: `ou=ecoles,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'ecoles',
+        },
+      });
+      res.end();
+    } else {
+      const ecoles = await prisma.school.findMany({
+        include: {
+          majors: {
+            include: {
+              students: true,
+            },
+          },
+        },
+      });
+      const ldapEcoles = ecoles.map((ecole) => {
+        return {
+          dn: `cn=${ecole.name},ou=ecoles,ou=groups,${rootDn}`,
+          attributes: {
+            cn: ecole.name,
+            displayName: ecole.name,
+            objectclass: ['top', 'groupOfNames'],
+            member: ecole.majors.map((major) => {
+              major.students.map((student) => {
+                return `cn=${student.uid},ou=people,${rootDn}`;
+              });
+            }),
+          },
+        };
+      });
+      for (const ecole of ldapEcoles.values()) res.send(ecole);
+      res.end();
+    }
+  } catch (error) {
+    console.error('Error handling search request:', error);
+    res.end(new ldap.OtherError(error.message));
+    return next(error);
+  }
+});
+
+server.search(`ou=aes,ou=groups,${rootDn}`, async (req, res, next) => {
+  console.log(`ou=aes,ou=groups,${rootDn} ` + req.scope);
+  try {
+    if (req.filter instanceof ldap.PresenceFilter && req.scope === 'base') {
+      res.send({
+        dn: `ou=ae,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'ae',
+        },
+      });
+      res.end();
+    } else {
+      const aes = await prisma.group.findMany({
+        where: {
+          type: 'StudentAssociationSection',
+        },
+        include: {
+          members: true,
+        },
+      });
+      const ldapAes = aes.map((ae) => {
+        return {
+          dn: `cn=${ae.name},ou=aes,ou=groups,${rootDn}`,
+          attributes: {
+            cn: ae.name,
+            displayName: ae.name,
+            description: ae.description,
+            objectclass: ['top', 'groupOfNames'],
+            memberUid: ae.members.map((member) => member.memberId),
+          },
+        };
+      });
+      for (const ae of ldapAes.values()) res.send(ae);
+      res.end();
+    }
+  } catch (error) {
+    console.error('Error handling search request:', error);
+    res.end(new ldap.OtherError(error.message));
+    return next(error);
+  }
+});
+
+server.search(`ou=clubs,ou=groups,${rootDn}`, async (req, res, next) => {
+  console.log(`ou=groups,ou=clubs,${rootDn} ` + req.scope);
+  try {
+    if (req.filter instanceof ldap.PresenceFilter && req.scope === 'base') {
+      res.send({
+        dn: `ou=groups,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'clubs',
+        },
+      });
+      res.end();
+    } else {
+      const clubs = await prisma.group.findMany({
+        where: {
+          type: 'Club' || 'Association',
+        },
+        include: {
+          members: true,
+        },
+      });
+      const ldapClubs = clubs.map((club) => {
+        return {
+          dn: `cn=${club.name},ou=clubs,ou=groups,${rootDn}`,
+          attributes: {
+            cn: club.name,
+            displayName: club.name,
+            description: club.description,
+            objectclass: ['top', 'groupOfNames'],
+            memberUid: club.members.map((member) => member.memberId),
+          },
+        };
+      });
+      for (const club of ldapClubs.values()) res.send(club);
+      res.end();
+    }
+  } catch (error) {
+    console.error('Error handling search request:', error);
+    res.end(new ldap.OtherError(error.message));
+    return next(error);
+  }
+});
+
+server.search(`ou=grp-informels,ou=groups,${rootDn}`, async (req, res, next) => {
+  console.log(`ou=grp-informels,ou=clubs,${rootDn} ` + req.scope);
+  try {
+    if (req.filter instanceof ldap.PresenceFilter && req.scope === 'base') {
+      res.send({
+        dn: `ou=grp-informels,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'grp-informels',
+        },
+      });
+      res.end();
+    } else {
+      const grp_informels = await prisma.group.findMany({
+        where: {
+          type: 'Group',
+        },
+        include: {
+          members: true,
+        },
+      });
+      const ldapGrpInformels = grp_informels.map((grp_informel) => {
+        return {
+          dn: `cn=${grp_informel.name},ou=grp-informels,ou=groups,${rootDn}`,
+          attributes: {
+            cn: grp_informel.name,
+            displayName: grp_informel.name,
+            description: grp_informel.description,
+            objectclass: ['top', 'groupOfNames'],
+            memberUid: grp_informel.members.map((member) => member.memberId),
+          },
+        };
+      });
+      for (const grp_informel of ldapGrpInformels.values()) res.send(grp_informel);
+      res.end();
+    }
+  } catch (error) {
+    console.error('Error handling search request:', error);
+    res.end(new ldap.OtherError(error.message));
+    return next(error);
+  }
+});
+
+server.search(`ou=groups,${rootDn}`, async (req, res, next) => {
+  console.log(`ou=groups,${rootDn} ` + req.scope);
+  try {
+    if (req.filter instanceof ldap.PresenceFilter && req.scope === 'base') {
+      res.send({
+        dn: `ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'groups',
+        },
+      });
+      res.end();
+    } else {
+      res.send({
+        dn: `ou=clubs,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'clubs',
+        },
+      });
+      res.send({
+        dn: `ou=ecoles,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'ecoles',
+        },
+      });
+      res.send({
+        dn: `ou=aes,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'ae',
+        },
+      });
+      res.send({
+        dn: `ou=grp-informels,ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'grp-informels',
+        },
+      });
+      res.end();
+    }
+  } catch (error) {
+    console.error('Error handling search request:', error);
+    res.end(new ldap.OtherError(error.message));
+    return next(error);
+  }
+});
+
 server.search(rootDn, async (req, res, next) => {
   console.log(rootDn + ' ' + req.scope);
   try {
@@ -237,6 +463,13 @@ server.search(rootDn, async (req, res, next) => {
         attributes: {
           objectclass: ['top', 'organizationalUnit'],
           ou: 'people',
+        },
+      });
+      res.send({
+        dn: `ou=groups,${rootDn}`,
+        attributes: {
+          objectclass: ['top', 'organizationalUnit'],
+          ou: 'groups',
         },
       });
       res.end();
